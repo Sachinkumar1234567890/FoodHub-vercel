ced59504766b0547a8df73b91ace141dfdc7deef
SachinKumar <ksachin0109@gmail.com>
2024-08-01 14:56:00 +0530

    Initial commit

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -27,3 +27,6 @@ urlpatterns = [
     path('',include('marketplace.urls')),
     path('admin/', admin.site.urls),
 ] +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+#     urlpatterns+=static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
